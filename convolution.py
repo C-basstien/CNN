6bbeb402cc438ff9d400f292d80e061596c9a1d3
Lagarde <sebastien.lagarde64@wanadoo.fr>
2017-11-21 22:52:30 +0100

    Update convolution.py

--- convolution.py
+++ convolution.py
@@ -4,14 +4,14 @@ import numpy as np
 #import matplotlib.image as mpimg
 from scipy import signal
 def convolve(im,ker):#fonction qui convolue 2 le ker et l'image (matrice)
-    im_width, im_height, RGB=im.shape
+    im_width, im_height, channel=im.shape
     #print(im.shape)
-    ker_width, ker_height,RGB, n_filtre=ker.shape
+    ker_width, ker_height,channel, n_filtre=ker.shape
     #print(ker.shape)
     out=np.zeros((im_width,im_height,n_filtre))
     buff=np.zeros((im_width+ker_width-1,im_height+ker_height-1,n_filtre))##create output vect
     for filter_index in range(n_filtre):
-        for color_index in range(3):#RGB mapping  
+        for color_index in range(channel):#RGB mapping  
             for i in range(im_width+ker_width):
                 print(i)
                 for j in range(im_height+ker_height):    
@@ -24,20 +24,20 @@ def convolve(im,ker):#fonction qui convolue 2 le ker et l'image (matrice)
                                  buff[i,j,filter_index]=buff[i,j,filter_index]+im[i-k,j-l,color_index]*ker[k,l,color_index,filter_index]
                               #                   if(i>ker_height )
  #                   else
-    out=buff[0:im_width,0:im_height,0:4]
+    out=buff[0:im_width,0:im_height,0:filter_index+1]
     return out
 def convolve2(im,ker):
-    im_width, im_height, RGB=im.shape
+    im_width, im_height, channel=im.shape
     #print(im.shape)
-    ker_width, ker_height,RGB, n_filtre=ker.shape
+    ker_width, ker_height,channel, n_filtre=ker.shape
     #print(ker.shape)
     out=np.zeros((im_width,im_height,n_filtre))
     buff=np.zeros((im_width+ker_width-1,im_height+ker_height-1,n_filtre))##create output vect
     for filter_index in range(n_filtre):
-        for color_index in range(3):#RGB mapping 
+        for color_index in range(channel):#RGB mapping 
            buff[:,:,filter_index]=buff[:,:,filter_index] + signal.convolve2d(im[:,:,color_index], ker[:,:,color_index,filter_index])
             #temp=signal.convolve2d(im[:,:,color_index], ker[:,:,color_index,filter_index])
-    out=buff[0:im_width,0:im_height,0:4]
+    out=buff[0:im_width,0:im_height,0:filter_index+1]
     return out
 
 def relu(vect):
@@ -66,30 +66,56 @@ def maxpool(vect):
                #out[i_x/stride,i_y/stride,i_channel]
                #np.amax(buff, axis=1)
     return out
-def reshape1 (vect,n):
+def reshape2 (vect,n):
     return np.reshape(vect,n)
+    
+
+def matrix_multilpy(Matrix,Vect): 
+    m_col, m_line = Matrix.shape
+    out=np.zeros((1,m_col))
+    for i_col in range(m_line):
+        for i_line in range(m_line):
+            out[1,i_col]=out[1,i_col]+Matrix[i_col,i_line]*Vect[1,i_line]
+    return out
+def matrix_multilpy2(Matrix,Vect):
+    return np.dot(Matrix,Vect)
+
+def softmax(vect):
+    return np.exp(vect)/(np.sum(np.exp(vect)))
+        
 #RGB=3
 #image=numpy.zeros((x_len,y_len,RGB))
 #img = mpimg.imread("image.jpg")
 image = Image.open("image 256 256.jpg")
 im_arr = np.array(image)
-print(im_arr.shape)
-print(im_arr)
-ker_arr = np.random.rand(3,3,3,1)#kernel de convolution mettre d'autre coeffs plus tard
+#print(im_arr.shape)
+#print(im_arr)
+filtre_number=64
+class_number=10
+ker_arr = np.random.rand(3,3,3,filtre_number)#kernel de convolution mettre d'autre coeffs plus tard
 #ker_arr = np.random.rand(3,3,3,64)#kernel de convolution mettre d'autre coeffs plus tard
-
-print(ker_arr.shape)
+matrix_classe=np.random.rand(class_number,16*16*filtre_number)
+ker_arr2 = np.random.rand(3,3,64,filtre_number)
+#print(ker_arr.shape)
 #print(ker_arr)
 #res_conv=convolve(im_arr,ker_arr)
 #print(res_conv)
-res_conv2=convolve2(im_arr,ker_arr)
-print("compar")
-print(res_conv2)
+#res_conv2=convolve2(im_arr,ker_arr)
+#print("compar")
+#print(res_conv2)
 #print(res_conv-res_conv2)
+res_conv2=convolve2(im_arr,ker_arr)
 relu_res=relu(res_conv2)
-maxp_res=maxpool(res_conv2)
-print(maxp_res)
-reshp_res=reshape1(maxp_res,64*64)
+maxp_res=maxpool(relu_res)
+res_conv22=convolve2(maxp_res,ker_arr2)
+relu_res2=relu(res_conv22)
+maxp_res2=maxpool(relu_res2)
+reschape_res=reshape2(maxp_res2,16*16*filtre_number)
+matrix_prod_res=matrix_multilpy2(matrix_classe,reschape_res)
+softmax_res=softmax(matrix_prod_res)
+print(softmax_res)
+#print(maxp_res)
+#reshp_res=reshape1(maxp_res,64*64*64)
 
 #print(res_conv2)
 #width, height = image.size
