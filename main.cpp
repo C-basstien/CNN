aa919e681f53bddccb2fc6a8284782b141bea82c
Lagarde <sebastien.lagarde64@wanadoo.fr>
2017-12-01 10:58:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <png.h>
+/*#include <jpeglib.h>
+#include <jerror.h>*/
+///#include <opencv2/opencv.hpp>
 using namespace std;
 /*void* new_multidimensional_matrix(const int dim_size_array[] )
 {
@@ -21,9 +27,78 @@ using namespace std;
     {
         if(sizeof(dim_size_array)>1){new_multidimensional_matrix(dim_size_array+1)}
     }
-
 }*/
 
+
+// Allocate imageData->pixels to be the correct size, start decompress and all
+// that jazz, like you did in your code. Skip allocating buffer though.
+// ...
+
+
+	
+
+//getPGMfile
+/*int *** readJpeg( char *path )
+{
+    FILE *file = fopen( path, "rb" );
+    if ( file == NULL )
+    {
+        return NULL;
+    }
+		
+
+    struct jpeg_decompress_struct info; //for our jpeg info
+    struct jpeg_error_mgr err; //the error handler
+
+    info.err = jpeg_std_error( &err );     
+    jpeg_create_decompress( &info ); //fills info structure
+
+    jpeg_stdio_src( &info, file );    
+    jpeg_read_header( &info, true );
+
+    jpeg_start_decompress( &info );
+
+		const int w = info.output_width;
+		const int h = info.output_height;
+		const int numChannels = info.num_components; // 3 = RGB, 4 = RGBA
+    unsigned long dataSize = w * h * numChannels;
+		
+		int *** im = new int **[w]();
+    for (int i = 0; i < w;i++)
+    {
+        im[i] = new int*[h]();
+        for (int j = 0; j < h;j++){im[i][j] = new int[numChannels]();}
+    }
+    // read RGB(A) scanlines one at a time into jdata[]
+    unsigned char *data = new unsigned char[dataSize]();
+    unsigned char* rowptr;
+    while ( info.output_scanline < h )
+    {
+        rowptr = data + info.output_scanline * w * numChannels;
+        jpeg_read_scanlines( &info, &rowptr, 1 );
+    }
+
+    jpeg_finish_decompress( &info );    
+
+    fclose( file );
+
+    // this code block is only if you happen to want output in CImg format, but is illustrative
+   // CImg<unsigned char> *image = new CImg<unsigned char>( w, h, 1, numChannels );
+    for ( int x = 0 ; x < w ; x++ )
+    {
+        for ( int y = 0 ; y < h ; y++ )
+        {
+            for ( int c = 0 ; c < numChannels ; c++ )
+            {
+                im[x][y][c] = data[ y * w * numChannels + x * numChannels + c ];
+            }
+        }
+    }
+
+    delete data ;
+
+    return im;
+}*/
 void* convolve(int*** im, int**** ker)
 {
 	/**Get the dimmension**/
@@ -88,6 +163,7 @@ void* convolve(int*** im, int**** ker)
 
 int main()
 {
+		//getPGMfile()
     cout << "Hello World!\n" << endl;
     return 0;
 }
