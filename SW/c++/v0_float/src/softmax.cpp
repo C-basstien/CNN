e1482ba9f5078ee19b8619948bd90b8af5a651e2
Lagarde <sebastien.lagarde64@wanadoo.fr>
2017-12-08 11:57:41 +0100

    Update softmax.cpp

--- SW/c++/v0_float/src/softmax.cpp
+++ SW/c++/v0_float/src/softmax.cpp
@@ -2,19 +2,14 @@
 
 double * softmax(double* V,unsigned int size)
 {
-    double* res= new double[size]();
-    double max=0;
+    double* res= new double[size]();/**output vector**/
     double norm=0;
-    for(unsigned int i=0;i<size;i++)
-    {
-            if(max<V[i]){max=V[i];}
-    }
-    for(unsigned int i=0;i<size;i++)
+    for(unsigned int i=0;i<size;i++)/**calcul exp of each element of the vector and the sum of all this exp elemnts**/
     {
         res[i]=exp(V[i]);
         norm=norm+res[i];
     }
-    for(unsigned int i=0;i<size;i++)
+    for(unsigned int i=0;i<size;i++)//division for each elements by the norm (which is the sum of all the exponentiate elements of the vector)
     {
         res[i]=res[i]/norm;
     }
