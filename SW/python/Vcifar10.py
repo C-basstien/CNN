2d6792d151aabe7d39640c1581da1a5cc1e4da2f
Lagarde <sebastien.lagarde64@wanadoo.fr>
2017-12-08 11:30:15 +0100

    Update Vcifar10.py

--- SW/python/Vcifar10.py
+++ SW/python/Vcifar10.py
@@ -3,67 +3,58 @@ import numpy as np
 #import matplotlib.pyplot as plt
 #import matplotlib.image as mpimg
 from scipy import signal
-def troncature(im):
-    im_width, im_height, channel=im.shape
+def troncature(im):#return a 24*24*3 image taken from the center part of the image
+    im_width, im_height, channel=im.shape#get the dimensions
     if (im_width < 24 or im_height < 24):
         print("Error, uncorrect image size")
         return
     else:
         im_tronc=np.zeros((24,24,channel))
-        im_tronc[:,:,:]=im[(im_width/2)-1-12:(im_width/2)-1+12,(im_height/2)-1-12:(im_height/2)-1+12,:]
+        im_tronc[:,:,:]=im[(im_width/2)-1-12:(im_width/2)-1+12,(im_height/2)-1-12:(im_height/2)-1+12,:]#take the image center
     return im_tronc
-def normalization(im):
+def normalization(im):#normalization function to modify according to tensorflow cifar 10 tutorial
     im_width, im_height, channel=im.shape
     mean=np.zeros(channel)
     delta=np.zeros(channel)
     im_norm=im
-    for i in range(channel):
+    for i in range(channel):#wrong the mean and variations calculation is done on all the image
         mean[i]=np.mean(im[:,:,i])
     for i in range(channel):
        delta[i]= sqrt(np.sum((im[:,:,i]-mean[i])*(im[:,:,i]-mean[i]))/(im_width*im_height))
        im_norm[:,:,i]=(im[:,:,i]-mean[1])/max(delta[i],1/sqrt(im_width*im_height))
     return im_norm
     
-def convolve(im,ker,biases):
-    im_width, im_height, channel=im.shape
-    #print(im.shape)
-    ker_width, ker_height,channel, n_filtre=ker.shape
-    #print(ker.shape)
-    out=np.zeros((im_width,im_height,n_filtre))
+def convolve(im,ker,biases):#convolution function including biases
+    im_width, im_height, channel=im.shape#get the dimensions of the input matrix
+    ker_width, ker_height,channel, n_filtre=ker.shape #get the dimensions of the input kernel
+    out=np.zeros((im_width,im_height,n_filtre))#initialization of output
     buff=np.zeros((im_width+ker_width-1,im_height+ker_height-1,n_filtre))##create output vect
-    for filter_index in range(n_filtre):
-        for color_index in range(channel):#RGB mapping 
+    for filter_index in range(n_filtre):#number of the filter 
+        for color_index in range(channel):#number of channel of the input matrix
            buff[:,:,filter_index]=buff[:,:,filter_index] + signal.convolve2d(im[:,:,color_index], ker[:,:,color_index,filter_index])
-            #temp=signal.convolve2d(im[:,:,color_index], ker[:,:,color_index,filter_index])
-    out=buff[0:im_width,0:im_height,0:filter_index+1]+biases[0:filter_index+1]
+    out=buff[0:im_width,0:im_height,0:filter_index+1]+biases[0:filter_index+1]#take the revelant part of the signal
     return out
 
-def relu(vect):
-    n_x, n_y, n_channel=vect.shape
+def relu(vect):#return a matrix with positive coefficient
+    n_x, n_y, n_channel=vect.shape#get the dimensions
     out=vect
     for i_x in range(n_x):
         for i_y in range(n_y):
             for i_channel in range(n_channel):
                 if(vect[i_x,i_y,i_channel] < 0):
-                    out[i_x,i_y,i_channel]=0
+                    out[i_x,i_y,i_channel]=0#if a a coefficent is negative then it's replace by 0
     return out
 def maxpool(vect):
-    n_x, n_y, n_channel = vect.shape
-   # print(n_x)
-    #print(n_y)
-    stride=2
-    size=3
-    buff=np.zeros((stride,stride))
+    n_x, n_y, n_channel = vect.shape#get the dimensions of the matrix
+    stride=2#step between zones of analyse
+    size=3#area of analyse 3*3blocs
+    buff=np.zeros((stride,stride))#initialization
     out=np.zeros((n_x/stride,n_y/stride,n_channel))
-    for i_channel in range(n_channel):
-        for i_x in range(0,n_x,stride):
+    for i_channel in range(n_channel):#number of channel in the matrix
+        for i_x in range(0,n_x,stride):#boucle of 
             for i_y in range(0,n_y,stride):
-               buff[:,:]= vect[i_x:i_x+size,i_y:i_y+size,i_channel]
-              # print(buff)
-               #print(buff.shape)
-               out[i_x/stride,i_y/stride,i_channel]=buff.max()
-               #out[i_x/stride,i_y/stride,i_channel]
-               #np.amax(buff, axis=1)
+               buff[:,:]= vect[i_x:i_x+size,i_y:i_y+size,i_channel]#get the local area of analisis
+               out[i_x/stride,i_y/stride,i_channel]=buff.max()#ge
     return out
 def reshape (vect,n):
     return np.reshape(vect,n)
@@ -72,6 +63,9 @@ def matrix_multilpy(Matrix,Vect):
 def softmax(vect):
     return (np.exp(vect))/(np.sum(np.exp(vect)))
         
+    
+#BENCH PARTS HERE
+#USE THE DIFFERENT FUNCTION TO TEST THE CIFAR10 LAYER
 #RGB=3
 #image=numpy.zeros((x_len,y_len,RGB))
 #img = mpimg.imread("image.jpg")
